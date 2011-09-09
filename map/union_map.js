79c7a3b0543374f008f8f931b25f6e1543934739
Andrew Browne <brownie@brownie.com.au>
2011-09-10 00:11:05 +1000

    Added union_map.js

---
+++ map/union_map.js
@@ -0,0 +1,53 @@
+function loadFile(map, number){
+  var colors = []
+    colors.push("#FF4848")
+    colors.push("#9669FE")
+    colors.push("#23819C")
+    colors.push("#01F33E")
+    colors.push("#DFE32D")
+    colors.push("#DFE32D")
+    colors.push("#FF800D")
+    colors.push("#4A9586")
+
+    $.getJSON("./union.js", function(data){
+      var parts = data;
+      var allCoords = []
+      var partCount = parts.length
+
+      for(var partIndex =0; partIndex < partCount; partIndex+=1){
+        var partCoords = []
+      var points = parts[partIndex];
+    var pointCount = points.length
+      for(var pointIndex = 0; pointIndex < pointCount; pointIndex+=1){
+        var point = points[pointIndex];
+        if(point != undefined){
+          partCoords.push(new google.maps.LatLng(point.y, point.x));
+        }
+      }
+      allCoords.push(partCoords);
+      }
+    bermudaTriangle = new google.maps.Polygon({
+      paths: allCoords,
+                    strokeColor: "#000000",
+                    strokeOpacity: 0.35,
+                    strokeWeight: 0.5,
+                    fillColor: colors[1],
+                    fillOpacity: 0.35
+    });
+
+    bermudaTriangle.setMap(map);
+
+      }
+    )
+}
+var latlng = new google.maps.LatLng(-29.000221252441406,141.00265502929688);
+var myOptions = {
+  zoom: 4,
+  center: latlng,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),
+    myOptions);
+
+loadFile(map)
+
