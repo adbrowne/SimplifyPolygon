96678a8987a232fcebc774a34abc2fe2e74c781e
Andrew Browne <brownie@browne.com.au>
2011-10-30 12:45:22 +1100

    Got class for reading dbf files

--- src/test/scala/ReadDbfFileSpec.scala
+++ src/test/scala/ReadDbfFileSpec.scala
@@ -4,38 +4,64 @@ import com.linuxense.javadbf._
 
 package net.hasnext.mapping.tests {
 
-import java.io.{FileInputStream, InputStream}
+  import java.io.{FileInputStream, InputStream}
 
-class ReadDbfFileSpec extends FlatSpec with ShouldMatchers {
-  "LoadDBFFile" should "open file" in{
-      var inputStream = new FileInputStream( "./data/australia/australia.dbf");
-       var reader = new DBFReader( inputStream);
-
-      // get the field count if you want for some reasons like the following
-      //
-      val numberOfFields = reader.getFieldCount();
-    var i = 0;
-    while(i< numberOfFields){
-        val field : DBFField = reader.getField( i);
-
-        // do something with it if you want
-        // refer the JavaDoc API reference for more details
-        //
-        System.out.println( field.getName());
-      i+=1;
+  class DbfReader(fileName: String) {
+    def getFieldNames(reader: DBFReader, index: Int, total: Int, fieldNames: List[String]) : List[String]= {
+      if(index == total){
+        fieldNames.reverse
+      }
+      else{
+        val field : DBFField = reader.getField(index)
+          val name = field.getName().trim() 
+          println(name)
+        getFieldNames(reader, index + 1, total, name :: fieldNames)
+      }
     }
-       var rowObjects : Array[Object] = Array()
-     while( rowObjects != null) {
-
-       var rowIterator = 0;
+    def getFieldNames(reader: DBFReader) : List[String] = {
+      val numberOfFields = reader.getFieldCount();
+      getFieldNames(reader, 0, numberOfFields, List())
+    }
+    def getRows(reader: DBFReader, fieldNames: List[String]) : List[Map[String, Object]] = {
+      var rows : List[Map[String, Object]] = List()
+        var rowObjects : Array[Object] = Array()
+        while( rowObjects != null) {
+        var row : Map[String, Object] = Map()
+          var rowIterator = 0;
         while( rowIterator< rowObjects.length) {
-
-          System.out.println( rowObjects(rowIterator));
+          val fieldName = fieldNames(rowIterator)
+            val fieldValue = rowObjects(rowIterator)
+            row += (fieldName -> fieldValue)
           rowIterator += 1;
         }
-       rowObjects = reader.nextRecord()
+        if(rowObjects.length > 0){
+          rows = row :: rows
+        }
+        rowObjects = reader.nextRecord()
       }
-    println(numberOfFields);
+      rows.reverse
+    }
+    def read = {
+      var inputStream = new FileInputStream(fileName);
+      var reader = new DBFReader(inputStream);
+
+      val fieldNames = getFieldNames(reader)
+
+        getRows(reader, fieldNames)
+
+    }
   }
- }
+
+  class ReadDbfFileSpec extends FlatSpec with ShouldMatchers {
+    "LoadDBFFile" should "open file" in{
+      val reader = new DbfReader("./data/australia/australia.dbf")
+        for (row <- reader.read)
+        println(row("ADMIN_NAME"))
+    }
+    "LoadDBFFile" should "open postcode file" in{
+      val reader = new DbfReader("./data/aus_postcodes/POA06aAUST_region.dbf")
+        for (row <- reader.read)
+        println(row("POA_2006"))
+    }
+  }
 }
