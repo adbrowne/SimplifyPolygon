56fe5391de8c3c3ad70e6b629491d2e9381ed239
Andrew Browne <brownie@brownie.com.au>
2011-12-17 03:06:38 +1100

    Implemented longest common substring

--- src/test/scala/LineEncodingSpec.scala
+++ src/test/scala/LineEncodingSpec.scala
@@ -1,52 +1,12 @@
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import net.hasnext.mapping._
 
 package net.hasnext.mapping.tests {
-  case class Point(val x: Float, val y: Float)
-
-  object Point {
-    def apply(point: Tuple2[Int,Int]) = {
-      point match {
-        case (x,y) => new Point(x,y)
-      }
-    } 
-
-  }
-  class Segment(val points : Seq[Point]){
-    def this() = this(List())
-    override def equals(other: Any) = other match {
-      case that: Segment =>
-        this.points == that.points
-      case _ =>
-        false
-    }
-  }
-
-  object Segment {
-    def apply(points: Tuple2[Int,Int]*) = {
-      new Segment(points map (x=> Point(x))) 
-    }
-  }
-  class MapRegion(val segments : List[Segment]) {
-    def this() = this(List())
-    }
-
-  object MapRegion{
-    def apply(points: Tuple2[Int,Int]*) = {
-      // TODO: Fix the duplication here. Should be able to call the segment singleton constructor
-      new MapRegion(List(new Segment(points map (x => Point(x)))))
-    }
-  }
-  class Map(val shapes: List[MapRegion]){
-    def this() = this(List())
-      def addShape(shape: MapRegion) : Map = {
-      new Map(shape::shapes)
-    }
-  }
 
   class MapSpec extends FlatSpec with ShouldMatchers {
     "AddShape" should "return map with single shape" in{
-      val newMap = new Map
+      val newMap = new PolyMap
       val mapWithOneShape = newMap.addShape(new MapRegion)
 
         mapWithOneShape.shapes.length should equal (1)
@@ -56,7 +16,7 @@ package net.hasnext.mapping.tests {
       val square1 = new MapRegion((0,0),(0,1),(1,1),(1,0))
         val square2 = new MapRegion(Lis((0,0),(0,1),(-1,1),(-1,0)))
 
-        val map = new Map(List(square1, square2))
+        val map = new PolyMap(List(square1, square2))
 
         map.segments.length should equal (3)
     }*/
@@ -73,7 +33,7 @@ package net.hasnext.mapping.tests {
     }
     
     "Segment" should "maintain point order" in {
-      Segment((0,0),(0,1)).points.head should equal (Point(0,0))
+      Segment((0,0),(0,1)).points.head should equal (MapPoint(0,0))
     }
   }
 
@@ -92,10 +52,57 @@ package net.hasnext.mapping.tests {
     }
   }
 
+  class LcsSpec extends FlatSpec with ShouldMatchers
+  {
+    def longestCommonSubstring[A](list1: Seq[A], list2: Seq[A]) : Seq[A] = {
+      // adapted from the haskell implementation here: 
+      // http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring
+      
+      def g[A](z: Seq[A], xy: Tuple2[A,A]) = {
+        xy match {
+          case (x,y) => if(x == y){
+                          z ++ List(x)
+                        }
+                        else{
+                          List()
+                        }
+        }
+      }
+      def f[A](xs: Seq[A], ys: Seq[A]) = {
+        xs.zip(ys).scanLeft(Seq[A]())(g)
+      }
+      val substrings = (for(xs <- list1.tails; ys <- list2.tails.drop(1))
+            yield(f(xs,list2) ++ f(list1,ys))).flatten
+
+      substrings.foldLeft(Seq[A]())(
+        (currentLongest, x) => {
+          if(x.length > currentLongest.length){
+            x
+          }
+          else{
+            currentLongest
+          }
+        })
+    }
+    "Single element" should "return single element" in {
+      longestCommonSubstring(List(1,2,3), List(3,4,5)) should equal (List(3)) 
+    }
+    "Two element" should "return two elements" in {
+      longestCommonSubstring(List(4,5,6), List(3,4,5)) should equal (List(4,5)) 
+    }
+  }
   class CommonSegmentSpec extends FlatSpec with ShouldMatchers
   {
+    def segmentOverlaps(segment1: Segment, segment2: Segment) = {
+      Segment((0,0), (0,1))
+    }
+
     "CommonSegment" should "identify common segments" in {
-      
+      val square1 = Segment((0,0),(0,1),(1,1),(1,0))
+      val square2 = Segment((0,0),(0,1),(-1,1),(-1,0))
+
+      val overlap = segmentOverlaps(square1, square2)
+      overlap should equal (Segment((0,0),(0,1))) 
     }
   }
 }
